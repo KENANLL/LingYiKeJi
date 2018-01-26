d32d6efb4acca92a61d3ef420efd4df19f708176
KENANLL <SRLiuLei@163.com>
2018-01-26 21:59:28 +0800

    测试

---
+++ 111/111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111
+//
+//  Created by 刘磊 on 2018/1/26.
+//  Copyright © 2018年 com.xiaoruikeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111/111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  111
+//
+//  Created by 刘磊 on 2018/1/26.
+//  Copyright © 2018年 com.xiaoruikeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
